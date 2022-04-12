2f646ac03a8ac8e9f1f7b841a048435caf738109
kalharko <oscar.dewasmes@gmail.com>
2022-04-12 23:30:31 +0200

    :sparkles: image with correct drag controls

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+# import hell to be delt with later
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty
+    NumericProperty, ReferenceListProperty, ObjectProperty, ListProperty, StringProperty
 )
 from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy import require
+from kivy.uix.image import Image
+
+#from PIL import Image
 
 require('2.1.0')
 
@@ -14,6 +18,36 @@ class Map(Widget):
     x = NumericProperty(0)
     y = NumericProperty(0)
     pos = ReferenceListProperty(x, y)
+    last_pos = ListProperty()
+
+    texture = StringProperty("")
+    size = ListProperty([0,0])
+
+    def load_texture(self, path) :
+        self.texture = path
+        im = Image(source=path)
+        self.size = im.texture_size
+        #self.allow_stretch = False
+        #self.keep_ratio = True
+
+    # # Controls
+    def on_touch_down(self, touch):
+        # Tracks position of touch down
+        self.last_pos = [touch.x, touch.y]
+
+        # Zoom in on the map
+        if touch.is_mouse_scrolling:
+            if touch.button == 'scrolldown':
+                self.size = (int(self.size[0]*0.9), int(self.size[1]*0.9))
+            elif touch.button == 'scrollup':
+                self.size = (int(self.size[0]*1.1), int(self.size[1]*1.1))
+
+    def on_touch_move(self, touch):
+        # Moves canvas following mouse drag
+        self.x += touch.x-self.last_pos[0]
+        self.y += touch.y-self.last_pos[1]
+        self.last_pos = touch.pos
+
 
 
 
@@ -23,14 +57,17 @@ class MainWidget(Widget):
     def update(self, dt): #dt as delta time ?
         pass
 
-    def on_touch_down(self, touch):
-        self.map.center = (0,0)
-        pass
+
+
 
 
 class VttApp(App):
     def build(self):
         mainWidget = MainWidget()
+
+        # map initialization
+        mainWidget.map.load_texture("Images/map_42x22.png")
+
         Clock.schedule_interval(mainWidget.update, 1.0 / 60.0)
         return mainWidget
 
@@ -40,3 +77,7 @@ if __name__ == '__main__':
 
 
 
+#  Image:
+#         center:root.center
+#         source: 'Images/map_42x22.png'
+#         pos: self.pos
